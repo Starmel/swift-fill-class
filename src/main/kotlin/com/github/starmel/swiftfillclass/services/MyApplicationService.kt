cbda93e87a7d51d7cfca7fc0a394c6b89f81c248
GitHub Action <action@github.com>
2022-07-25 19:50:00 +0000

    Template cleanup

--- src/main/kotlin/com/github/starmel/swiftfillclass/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/starmel/swiftfillclass/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.starmel.swiftfillclass.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.starmel.swiftfillclass.MyBundle
 
 class MyApplicationService {
 
